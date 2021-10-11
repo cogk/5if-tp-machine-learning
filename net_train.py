8e109d483d7b57e2ef32afc2767d185b1944a054
Corentin Flr <10946971+cogk@users.noreply.github.com>
2021-10-12 01:06:13 +0200

    global loss print

--- net_train.py
+++ net_train.py
@@ -19,22 +19,28 @@ import time
 
 def load_train_save(net):
     net.load_from_file()
-    net_train(net, save_after_epoch=True)
+    try:
+        net_train(net, save_between_epochs=True)
+    except:
+        return net
+
     net.save_to_file()
     return net
 
 
-def net_train(net, save_after_epoch=False):
+def net_train(net, save_between_epochs=False):
     print('Starting Training')
     n_epochs = 2
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    for epoch in range(0, n_epochs):
-        if save_after_epoch:
-            net.save_to_file()
+    delta_losses = [0]
+    all_losses = []
 
+    for epoch in range(0, n_epochs):
+        print('Start of Epoch', epoch + 1, '/', n_epochs)
+        epoch_loss = 0.0
         running_loss = 0.0
 
         for i, data in enumerate(train_loader, 0):
@@ -50,11 +56,28 @@ def net_train(net, save_after_epoch=False):
             optimizer.step()
 
             # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
+            l = loss.item()
+            running_loss += l
+            epoch_loss += l
+
+            if i % 1000 == 999:  # print every 1000 mini-batches
                 print('[e=%d, i=%5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
+
+        print('End of Epoch %d: loss: %.3f' %
+              (epoch + 1, epoch_loss / 2000))
+
+        if epoch > 0:
+            delta_losses.append(all_losses[epoch - 1] - running_loss)
+        all_losses.append(running_loss)
+
+        if delta_losses[-1] > 0:
+            print('/!\\ loss is increasing')
+            raise 'loss is increasing'
+
+        if save_between_epochs and epoch < n_epochs - 1:
+            net.save_to_file()
 
     print('Finished Training')
     return net
