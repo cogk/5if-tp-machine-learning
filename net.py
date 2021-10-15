d438da26efe60e43ffe8f9c339f684fb03d33431
Corentin Flr <10946971+cogk@users.noreply.github.com>
2021-10-15 22:26:10 +0200

    udpated network structure (added dropout)

--- net.py
+++ net.py
@@ -12,13 +12,14 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(16 * 6 * 6, 32)
         self.fc2 = nn.Linear(32, 16)
         self.fc3 = nn.Linear(16, 2)
+        self.dropout = nn.Dropout(p=0.4)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, 16 * 6 * 6)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.dropout(self.fc1(x)))
+        x = F.relu(self.dropout(self.fc2(x)))
         x = self.fc3(x)
         return x
 
