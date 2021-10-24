7e3fa82b44d2dbff45da40330fc20357bff9d3d9
Corentin Flr <10946971+cogk@users.noreply.github.com>
2021-10-24 20:32:42 +0200

    fixed data loader

--- load_data.py
+++ load_data.py
@@ -5,49 +5,58 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
-train_dir = './train_images'
-# train_dir_extra = './train_images_extra'
-train_dir_lfw = './train_images_lfw-deepfunneled'
-train_dir_kaggle_nat_img = './train_images_kaggle'
-test_dir = './test_images'
 
 transform = transforms.Compose(
     [transforms.Grayscale(),
      transforms.ToTensor(),
      transforms.Normalize(mean=(0,), std=(1,))])
 
-train_data_original = torchvision.datasets.ImageFolder(
-    train_dir, transform=transform)
-# train_data_extra = torchvision.datasets.ImageFolder(
-#     train_dir_extra, transform=transform)
-train_data_lfw = torchvision.datasets.ImageFolder(
-    train_dir_lfw, transform=transform)
-train_data_kaggle_nat_img = torchvision.datasets.ImageFolder(
-    train_dir_kaggle_nat_img, transform=transform)
+folders = [
+    './train_images',
+    # './train_images_extra',
+    './train_images_lfw-deepfunneled',
+    './train_images_kaggle',
+    './test_images',
+]
+datasets = [
+    torchvision.datasets.ImageFolder(folder, transform=transform)
+    for folder in folders
+]
+all_data = torch.utils.data.ConcatDataset(datasets)
 
-train_data = torch.utils.data.ConcatDataset(
-    [train_data_original, train_data_lfw, train_data_kaggle_nat_img])
+# test_data = torchvision.datasets.ImageFolder(test_dir, transform=transform)
 
-test_data = torchvision.datasets.ImageFolder(test_dir, transform=transform)
+valid_size = 0.1
+test_size = 0.1
+train_size = 1 - valid_size - test_size
+batch_size = 64
 
-valid_size = 0.2
-batch_size = 32
+num = len(all_data)
+indices = list(range(num))
 
-num_train = len(train_data)
-indices_train = list(range(num_train))
-np.random.shuffle(indices_train)
-split_tv = int(np.floor(valid_size * num_train))
-train_new_idx, valid_idx = indices_train[split_tv:], indices_train[:split_tv]
+np.random.seed(42)  # Don't forget to seed
+np.random.shuffle(indices)
 
-# train_sampler = SubsetRandomSampler(train_new_idx)
-train_sampler = ImbalancedDatasetSampler(train_data, train_new_idx)
+split1 = int(np.floor(valid_size * num))
+split2 = int(np.floor((valid_size + test_size) * num))
+valid_idx = indices[:split1]
+test_idx = indices[split1:split2]
+train_idx = indices[split2:]
 
-valid_sampler = SubsetRandomSampler(valid_idx)
+print('Datasets:')
+print('- valid:', len(valid_idx))
+print('- test :', len(test_idx))
+print('- train:', len(train_idx))
+
+valid_sampler = ImbalancedDatasetSampler(all_data, valid_idx)
+test_sampler = ImbalancedDatasetSampler(all_data, test_idx)
+train_sampler = ImbalancedDatasetSampler(all_data, train_idx)
 
 train_loader = torch.utils.data.DataLoader(
-    train_data, batch_size=batch_size, sampler=train_sampler, num_workers=1)
+    all_data, batch_size=batch_size, sampler=train_idx, num_workers=2)
 valid_loader = torch.utils.data.DataLoader(
-    train_data, batch_size=batch_size, sampler=valid_sampler, num_workers=1)
+    all_data, batch_size=batch_size, sampler=valid_idx, num_workers=2)
 test_loader = torch.utils.data.DataLoader(
-    test_data, batch_size=batch_size, shuffle=True, num_workers=1)
+    all_data, batch_size=batch_size, sampler=test_idx, num_workers=2)
+
 classes = ('noface', 'face')
