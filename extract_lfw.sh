99d88661cc11bba89125d7735602d35337b61f40
Corentin Flr <10946971+cogk@users.noreply.github.com>
2021-10-24 20:32:33 +0200

    improved LFW extraction

--- extract_lfw.sh
+++ extract_lfw.sh
@@ -7,15 +7,18 @@ mkdir -p "$out" "$out/0" "$out/1"
 crop() {
   img="$1"
   local n="$(basename "${img%%.*}")"
+  
   convert "$img" \
     -resize 72x72 -crop 36x36+18+18 \
     -set colorspace Gray -separate -average \
     "$out/1/$n.pgm"
+
   convert "$img" \
-    -resize 252x252 \
+    -resize 180x180 \
     -set colorspace Gray -separate -average \
     -crop 36x36 \
     "$out/0/${n}_%d.pgm"
+  rm "$out/0/${n}_"{6,7,8,11,12,13,16,17,18}".pgm"
 }
 
 for person_dir in "$in_dir"/*; do
